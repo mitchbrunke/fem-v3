f65a4ced983902e3f87821623de29b8ae7865b2b
Mitch Brunke <mbrunke@fcte.com.au>
2020-12-09 16:46:15 +1000

    head bang here

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./styles/styles.scss";
 
-console.log("hello world!");
+const navToggle = document.querySelector(".nav__toggle");
+const mobNav = document.querySelector(".mob-contain");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+  if (document.body.classList.contains("nav-open")) {
+    console.log("open");
+    mobNav.style.display = "block";
+  } else {
+    mobNav.style.display = "none";
+    console.log("closed");
+  }
+});
